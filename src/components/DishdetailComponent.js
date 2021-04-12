ecb0a1b4c2e6134cafb78720fe10e95bca357813
joacoferreiros <joacoferreiros@gmail.com>
2021-04-12 11:38:13 +0200

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -29,7 +29,7 @@ class DishDetail extends Component {
                             return (
                                 <li key={comment.id}>
                                     <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {comment.date}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                                 </li>
                             );
                         })}
@@ -45,9 +45,11 @@ class DishDetail extends Component {
     render() {
         if(this.props.dish != null)
             return (
+                <div className="container">
                 <div className="row">
                     {this.renderDish(this.props.dish)}
                     {this.renderComments(this.props.dish.comments)}
+                </div>
                 </div>                      
                     
             );
